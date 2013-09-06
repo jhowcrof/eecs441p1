08a1454fb53677bd971a8669740b401c8665c60d
johnzk <johnzk@umich.edu>
2013-09-06 15:30:59 -0400

    Hello World

--- EECS441P1/ViewController.m
+++ EECS441P1/ViewController.m
@@ -6,7 +6,8 @@
 //  Copyright (c) 2013 Jacob Howcroft. All rights reserved.
 //
 
-// This is the first commit.  Hello World. 
+// This is the first commit.  Hello World.
+// Second commit Hello!
 
 #import "ViewController.h"
 
